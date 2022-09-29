7f01f349924d6fb7c3a0ea7cdc51c41901407b14
lynix28 <lynixizunia@gmail.com>
2022-09-29 21:43:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-testcase=$1
-discord_token=$2
-discord_webhookid=$3
+# "pipeline" or "local"
+run_on=$1
+
+# name of test script
+testcase=$2
+
+# notification config "teams" or "discord"
+notification=$3
+
+# discord notification token
+discord_token=$4
+discord_webhookid=$5
+
 date=$(date "+%Y%m%d_%H%M%S")
 run_counter=0
 
@@ -23,12 +33,76 @@ function send_notif () {
     fi
 }
 
-function run_k6 () {
-    cd /app
-    k6 run $testcase -c ./config.json --log-output file=reports/k6_$date.log --log-format json --summary-export ./reports/report_$date.json -q
-    run_counter=1
+function help () {
+    echo "format: ./run.sh [local / pipeline] [test script] [teams / discord] [token] [webhookid]"
+    echo " "
+    echo "sample:"
+    echo "local    -> ./run.sh local test.js"
+    echo "pipeline -> ./run.sh pipeline test.js discord 12345 67890"
 }
 
-send_notif
+function check_local () {
+    if [ ! -z "$testcase" ] && [ -z "$notification" ] && [ -z "$discord_token" ] && [ -z "$discord_webhookid" ]
+    then
+        result=0
+    else
+        result=1
+    fi
+}
+
+function check_pipeline () {
+    if [ ! -z "$testcase" ] && [ ! -z "$notification" ] && [ ! -z "$discord_token" ] && [ ! -z "$discord_webhookid" ]
+    then
+        result=0
+    fi
+    if [ -z "$testcase" ]
+    then
+        result=1
+    fi
+    if [ -z "$notification" ]
+    then
+        result=1
+    fi
+    if [ -z "$discord_token" ]
+    then
+        result=1
+    fi
+    if [ -z "$discord_webhookid" ]
+    then
+        result=1
+    fi
+}
+
+function run_k6 () {
+    if [ "$run_on" == "local" ]
+    then
+        check_local
+        if [ $result -eq 0 ]
+        then
+            cd /app
+            k6 run $testcase -c ./config.json --log-output file=reports/k6_$date.log --log-format json --summary-export ./reports/report_$date.json
+        else
+            echo "invalid command"
+            help
+        fi
+    elif [ "$run_on" == "pipeline" ]
+    then
+        check_pipeline
+        if [ $result -eq 0 ]
+        then
+            send_notif
+            cd /app
+            k6 run $testcase -c ./config.json --log-output file=reports/k6_$date.log --log-format json --summary-export ./reports/report_$date.json -q
+            run_counter=1
+            send_notif
+        else
+            echo "invalid command"
+            help
+        fi
+    else
+        echo "invalid command"
+        help
+    fi
+}
+
 run_k6
-send_notif
